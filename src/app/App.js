ee554936242a18abb45360454356fdb023471e1d
ZackDinerstein <zackdhome@gmail.com>
2019-04-23 12:23:14 +0300

    Allowed React to build cards

--- src/app/App.js
+++ src/app/App.js
@@ -4,12 +4,44 @@ import './App.scss';
 import '../card/Card.scss';
 import { Card } from '../card/Card.js';
 
-function App() {
-  return (
-    <div className="card-container">
-      <Card />
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      numOfCards: 5
+    };
+  }
+
+  renderCards(){
+    let cards = [];
+
+    for (let i = 0; i < this.state.numOfCards; i++){
+      cards.push(this.renderCard(i))
+    }
+
+    return (
+      cards
+    )
+  }
+
+  renderCard(i){
+    return (
+      <Card key={i} />
+    );
+  }
+
+
+  render(){
+    return (
+      <div className="card-container">
+        <div className="row">
+          {this.renderCards()}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
