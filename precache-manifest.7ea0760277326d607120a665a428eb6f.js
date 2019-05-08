6e7eb6214b35c4701c579fa54d27540c85d1a779
ZackDinerstein <zackdhome@gmail.com>
2019-05-08 11:52:14 +0300

    Updates

---
+++ precache-manifest.7ea0760277326d607120a665a428eb6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d7c5181ec0bc7b422f59be7dd68303",
+    "url": "/challenge--honeybook-react-app/index.html"
+  },
+  {
+    "revision": "c8d06d5a7105997817eb",
+    "url": "/challenge--honeybook-react-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e32b959d5845c6f0e3a6",
+    "url": "/challenge--honeybook-react-app/static/css/main.ed603762.chunk.css"
+  },
+  {
+    "revision": "c8d06d5a7105997817eb",
+    "url": "/challenge--honeybook-react-app/static/js/2.11d013f8.chunk.js"
+  },
+  {
+    "revision": "e32b959d5845c6f0e3a6",
+    "url": "/challenge--honeybook-react-app/static/js/main.f1955c90.chunk.js"
+  },
+  {
+    "revision": "09eb2ddfa166328d5c1b",
+    "url": "/challenge--honeybook-react-app/static/js/runtime~main.6d3ef001.js"
+  }
+]);
